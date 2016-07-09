2ba72dcec6f1b73ce80938c26d8048b0892a3c2b
mr-augustine <pushbuttonrobotics@gmail.com>
2016-07-09 17:42:29 -0500

    Initial commit

---
+++ demo_odometer/kintobor.h
@@ -0,0 +1,24 @@
+/*
+ * file: kintobor.h
+ * created: 20160708
+ * author(s): mr-augustine
+ *
+ * The kintobor header file lists function prototypes for all of the robot's
+ * higher-order functions.
+ */
+#ifndef _KINTOBOR_H_
+#define _KINTOBOR_H
+
+#include "odometer.h"
+#include "uwrite.h"
+
+#define ROBOT_NAME          ("kintobor")
+#define METERS_PER_TICK     0.000012345
+
+#ifdef __cplusplus
+extern "C" {
+
+}
+#endif // #ifdef __cplusplus
+
+#endif // #ifndef _KINTOBOR_H_
