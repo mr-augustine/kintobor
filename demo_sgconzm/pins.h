f32e7caec2b49f7bd2255876ae68533335b1e21d
mr-augustine <pushbuttonrobotics@gmail.com>
2016-09-06 22:28:35 -0500

    Add mobility pin definitions
    Everything compiles

--- demo_sgconzm/pins.h
+++ demo_sgconzm/pins.h
@@ -44,6 +44,18 @@
 #define BUTTON_LED_PIN      PH6     // Mega Digital Pin 9
 
 ////////////////////////////////////////////////////////////////////////////////
+// MOBILITY
+#define STEERING_PORT       PORTE
+#define STEERING_DDR        DDRE
+#define STEERING_PINVEC     PINE
+#define STEERING_PIN        PE4     // Mega Digital Pin 2
+
+#define THROTTLE_PORT       PORTE
+#define THROTTLE_DDR        DDRE
+#define THROTTLE_PINVEC     PINE
+#define THROTTLE_PIN        PE5     // Mega Digital Pin 3
+
+////////////////////////////////////////////////////////////////////////////////
 // ODOMETER
 #define ODOMETER_PORT       PORTD
 #define ODOMETER_DDR        DDRD
